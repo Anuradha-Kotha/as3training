2db81e42422f1013d2247457d2f810e0353f0d11
Anuradha Kotha <radha.anu95@gmail.com>
2022-09-16 09:06:28 -0700

    Fibonacci.java: It will print numbers in fibonacci series. Print.java: Printing name.

---
+++ src/main/java/org/com/practice/Fibonacci.java
@@ -0,0 +1,27 @@
+package org.com.practice;
+
+public class Fibonacci {
+    public static void main(String[] args){
+         int n = 10 ;
+         int n1=0;
+         int n2=1;
+        // int i = 0;
+
+         System.out.print(n1 + " ");
+         System.out.print(n2 + " ");
+
+         for(int i=0;i<n-2;i++){
+             int n3 = n1+n2;
+            System.out.print(n3 +" ");
+             n1=n2;
+             n2=n3;
+
+         }
+
+
+
+
+
+
+    }
+}
---
+++ src/main/java/org/com/practice/Print.java
@@ -0,0 +1,7 @@
+package org.com.practice;
+
+public class Print {
+    public static void main(String[] args) {
+        System.out.println("Anuradha");
+    }
+}
